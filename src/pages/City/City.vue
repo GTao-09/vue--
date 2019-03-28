49b57fdef6d64737a3efef0fd773b9bb13e7bccc
GTao-09 <gongtao1997@gmail.com>
2019-03-28 22:33:55 +0800

    搜索逻辑

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="City">
     <CityHeader></CityHeader>
-    <CitySearch></CitySearch>
+    <CitySearch  :cities="cities"></CitySearch>
     <CityList :hotCities="hotCities" :cities="cities" :letter="letter"></CityList>
     <CityAlphabet :cities="cities" @change="handlerLetterChange"></CityAlphabet>
   </div>
--- src/pages/City/components/Search.vue
+++ src/pages/City/components/Search.vue
@@ -1,17 +1,63 @@
 <template>
-  <dir class="CitySearch">
-    <input type="search" name="search" class="city-search" placeholder="输入城市名或拼音">
-  </dir>
+  <div>
+    <div class="CitySearch">
+      <input v-model="keyword" type="search" class="city-search" placeholder="输入城市名或拼音">
+    </div>
+    <div v-show="keyword" class="search-content" ref="search1">
+      <ul>
+        <li class="content border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li v-show="!list.length" class="nosearch"> 没有匹配到 </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      TimeOut: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.TimeOut) {
+        clearTimeout(this.TimeOut)
+      }
+      if (!this.keyword) {
+        this.list = []
+      }
+      this.TimeOut = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach(element => {
+            if (element.spell.indexOf(this.keyword) > -1 || element.name.indexOf(this.keyword) > -1) {
+              result.push(element)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search1)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '../../../assets/styles/varibles.styl'
+.border-bottom
+  &:after
+    border-color #cccccc
 .CitySearch
   height .72rem
   background-color $bgColor
@@ -22,6 +68,21 @@ export default {
     width 100%
     text-align center
     box-sizing border-box
-    padding 0 .3rem
+    padding 0 .2rem 0 .5rem
     border-radius .1rem
+.search-content
+  overflow hidden
+  z-index 1
+  position absolute
+  top 1.58rem
+  right 0
+  left 0
+  bottom 0
+  background-color #eeeeee
+  .content
+  .nosearch
+    height .54rem
+    line-height .54rem
+    background-color #ffffff
+    color #666666
 </style>
