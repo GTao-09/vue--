6515fc4e98483aa329d8455bfd752c8a81424136
GTao-09 <gongtao1997@gmail.com>
2019-03-28 21:22:59 +0800

    联动性能优化

--- src/pages/City/components/Alphabet.vue
+++ src/pages/City/components/Alphabet.vue
@@ -22,8 +22,13 @@ export default {
   },
   data () {
     return {
-      touchState: false
+      touchState: false,
+      ALetterTop: 0,
+      TimeOut: null
     }
+  },
+  updated () {
+    this.ALetterTop = this.$refs['A'][0].offsetTop
   },
   computed: {
     letters () { // 将cities中的字母提出来组成一个数组
@@ -43,17 +48,30 @@ export default {
       this.touchState = true
     },
     handlerTouchMove (e) {
+      // if (this.touchState) {
+      //   const ALetterTop = this.$refs['A'][0].offsetTop // A字母离顶端的距离
+      //   // console.log(ALetterTop)
+      //   // console.log(Math.round(e.touches[0].clientY))
+      //   const ClientY = Math.round(e.touches[0].clientY) - 79 // 滑动位置离字幕条顶端的距离
+      //   const letterIndex = Math.round((ClientY - ALetterTop) / 20) // 20为每个字母的高度;letterIndex滑动位置的字母位置
+      //   // console.log(letterIndex)
+      //   if (letterIndex >= 0 && letterIndex < this.letters.length) {
+      //     // console.log(this.letters[letterIndex])
+      //     this.$emit('change', this.letters[letterIndex])
+      //   }
+      // }
       if (this.touchState) {
-        const ALetterTop = this.$refs['A'][0].offsetTop // A字母离顶端的距离
-        // console.log(ALetterTop)
-        // console.log(Math.round(e.touches[0].clientY))
-        const ClientY = Math.round(e.touches[0].clientY) - 79 // 滑动位置离字幕条顶端的距离
-        const letterIndex = Math.round((ClientY - ALetterTop) / 20) // 20为每个字母的高度;letterIndex滑动位置的字母位置
-        // console.log(letterIndex)
-        if (letterIndex >= 0 && letterIndex < this.letters.length) {
-          // console.log(this.letters[letterIndex])
-          this.$emit('change', this.letters[letterIndex])
+        // 利用函数节流对函数进行优化
+        if (this.TimeOut) {
+          clearTimeout(this.TimeOut)
         }
+        this.TimeOut = setTimeout(() => {
+          const ClientY = Math.round(e.touches[0].clientY) - 79
+          const letterIndex = Math.round((ClientY - this.ALetterTop) / 20)
+          if (letterIndex >= 0 && letterIndex < this.letters.length) {
+            this.$emit('change', this.letters[letterIndex])
+          }
+        }, 16)
       }
     },
     handlerTouchEnd () {
