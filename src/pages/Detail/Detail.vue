9195dcfe03607f75968e80132de96c77138ebb42
GTao-09 <gongtao1997@gmail.com>
2019-03-30 21:48:55 +0800

    递归组件

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -2,18 +2,45 @@
   <div class="Detail">
     <DetailHeader></DetailHeader>
     <DetailTop></DetailTop>
-    <div style="height: 100rem;">演示Top效果</div>
+    <DetailList :list="list"></DetailList>
+    <!-- <div style="height: 100rem;">演示Top效果</div> -->
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Top from './components/Top'
+import List from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailHeader: Header,
-    DetailTop: Top
+    DetailTop: Top,
+    DetailList: List
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [{
+            title: '成人票价格'
+          }]
+        },
+        {
+          title: '学生票',
+          children: [{
+            title: '学生票价格'
+          }]
+        },
+        {
+          title: '儿童票',
+          children: [{
+            title: '儿童票价格'
+          }]
+        }
+      ]
+    }
   }
 }
 </script>
--- src/pages/Detail/components/Top.vue
+++ src/pages/Detail/components/Top.vue
@@ -61,6 +61,7 @@ export default {
     top 0
     left 0
     right 0
+    z-index 1
     .Top-title
       line-height $HeaderHeight
       text-align center
