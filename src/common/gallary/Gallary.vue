2ef4ed46d4c05b4cfebaceafa95c7379608599f0
GTao-09 <gongtao1997@gmail.com>
2019-03-30 18:49:33 +0800

    detailHeader中引入Gallary组件

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="Gallary" @click="NoshowGallary">
+    <div class="warp">
+     <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) in imgGallary" :key="index">
+          <img class="imgstyle" :src="item">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallary',
+  props: {
+    imgGallary: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observeParents: true
+      }
+    }
+  },
+  methods: {
+    NoshowGallary () {
+      this.$emit('closed')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.Gallary >>> .swiper-container
+  overflow inherit
+.Gallary
+  display flex
+  flex-direction column
+  justify-content center
+  position fixed
+  right 0
+  left 0
+  top 0
+  bottom 0
+  background-color #000000
+  .warp
+    // overflow hidden
+    height 0
+    width 100%
+    padding-bottom 70%
+    .imgstyle
+      width 100%
+    .swiper-pagination
+      bottom -1rem
+      color #ffffff
+</style>
--- src/pages/Detail/components/Header.vue
+++ src/pages/Detail/components/Header.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="DetailHeader">
-    <img class="detail-img" src="//img1.qunarzz.com/sight/p0/1412/77/cb5a5c0c47e96f6b42ce646cddf4af0d.water.jpg_600x330_17d4b9aa.jpg">
-    <div class="detailHeader-info">
-      <div class="detail-title">鸟巢(AAAAAA景区)</div>
-      <div class="detail-imgNumber">
-        <span class="iconfont imgfont">&#xe640;</span>6
+  <div>
+    <div class="DetailHeader" @click="handleShowGallary">
+      <img class="detail-img" src="//img1.qunarzz.com/sight/p0/1412/77/cb5a5c0c47e96f6b42ce646cddf4af0d.water.jpg_600x330_17d4b9aa.jpg">
+      <div class="detailHeader-info">
+        <div class="detail-title">鸟巢(AAAAAA景区)</div>
+        <div class="detail-imgNumber">
+          <span class="iconfont imgfont">&#xe640;</span>6
+        </div>
       </div>
     </div>
+    <Gallary v-show="showGallary" @closed="handleNoShowGallary" :imgGallary="imgGallary"></Gallary>
   </div>
 </template>
 
 <script>
+import Gallary from '../../../common/gallary/Gallary'
 export default {
-  name: 'DetailHeader'
+  name: 'DetailHeader',
+  components: {
+    Gallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgGallary: [
+        'http://img1.qunarzz.com/sight/p0/1508/2c/e8136f95b4ce7866337c72730167b1a3.water.jpg_r_800x800_d8ac3143.jpg',
+        'http://img1.qunarzz.com/sight/p0/1808/ac/aca2bb073359bb0aa3.water.jpg_r_800x800_224b231f.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleShowGallary () {
+      this.showGallary = true
+    },
+    handleNoShowGallary () {
+      this.showGallary = false
+    }
+  }
 }
 </script>
 
